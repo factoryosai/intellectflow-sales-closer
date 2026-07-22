feb47356593fc69817362bdc335c0c53679c12a9
factoryosai <factoryosai@gmail.com>
2026-07-22 18:13:11 +0530

    Update maps.ts

--- lib/maps.ts
+++ lib/maps.ts
@@ -1,1 +1,14 @@
-
+export async function searchPlaces(keyword: string, location: string = "Rajkot") {
+  const url = "https://places.googleapis.com/v1/places:searchText";
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Goog-Api-Key": process.env.GOOGLE_PLACE_API_KEY!,
+      "X-Goog-FieldMask": "places.displayName,places.rating,places.userRatingCount,places.formattedAddress,places.nationalPhoneNumber,places.websiteUri,places.types,places.reviews"
+    },
+    body: JSON.stringify({ textQuery: `${keyword} in ${location}`, maxResultCount: 20 })
+  });
+  const data = await res.json();
+  return data.places || [];
+}
